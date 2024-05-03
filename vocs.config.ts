a39e2eccf56e3e83ffb430d290ddd5af9c10b4ed
Lukas Rosario <36800180+lukasrosario@users.noreply.github.com>
2024-05-03 07:36:42 -0400

    add some docs

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,28 +1,102 @@
 import { defineConfig } from 'vocs'
 
 export default defineConfig({
+  title: 'ERC-7677',
+  titleTemplate: '%s · ERC-7677',
+  description: 'Standards for unlocking gasless experiences',
   theme: {
-    colorScheme: 'system',
+    accentColor: {
+      light: '#73F7FF',
+      dark: '#73F7FF',
+    },
     variables: {
       color: {
-        textAccent: { light: '#2394ff', dark: '#e9e9ea' },
-        backgroundDark: {light: '#ebeaee', dark: 'black'},
-        background: {light: '#f6f5f8', dark: 'black'}, //f6f5f8 /ebf1f8
-        heading: { light: 'black', dark: '#e9e9ea' },
-        text3: { light: 'black', dark: '#e9e9ea' },
-        background5: { light: '#dee8ff', dark: '#3c393f' },
-      }
-    }
+        background: {
+          light: 'white',
+          dark: '#151A26',
+        },
+        backgroundDark: {
+          light: '#F5F5F5',
+          dark: '#0F131E',
+        },
+        textAccent: {
+          light: '#050F19',
+          dark: 'white',
+        },
+      },
+    },
   },
-  title: 'ERC-7677',
+  topNav: [ 
+    { text: 'ERC', link: 'https://github.com/ethereum/ERCs/pull/360' }, 
+  ], 
   sidebar: [
+    {
+      text: 'Introduction',
+      link: '/introduction',
+    },
     {
       text: 'Getting Started',
       link: '/getting-started',
     },
     {
-      text: 'Example',
-      link: '/example',
+      text: 'Guides',
+      collapsed: false,
+      items: [
+        {
+          text: 'Setting up a paymaster service proxy',
+          link: '/guides/setting-up-paymaster-proxy',
+        },
+        {
+          text: 'Constructing a user operation with ERC-7677',
+          link: '/guides/construct-user-op-with-7677',
+        }
+      ]
+    },
+    { 
+      text: 'Reference', 
+      collapsed: false, 
+      items: [ 
+        {
+          text: 'Paymaster Services',
+          items: [
+            {
+              text: 'pm_getPaymasterStubData',
+              link: '/reference/paymasters/getPaymasterStubData'
+            },
+            {
+              text: 'pm_getPaymasterData',
+              link: '/reference/paymasters/getPaymasterData'
+            }
+          ]
+        },
+        {
+          text: 'Wallets',
+          items: [
+            {
+              text: 'wallet_getCapabilities Behavior',
+              link: '/reference/wallets/getCapabilities'
+            },
+          ]
+        },
+      ], 
+    },
+    { 
+      text: 'Ecosystem', 
+      collapsed: false, 
+      items: [ 
+        { 
+          text: 'Paymaster Service Providers', 
+          link: '/ecosystem/paymasters', 
+        },
+        { 
+          text: 'Wallets', 
+          link: '/ecosystem/wallets', 
+        },
+        {
+          text: 'Tools',
+          link: '/ecosystem/tools',
+        },
+      ], 
     },
   ],
 })
